d79401a8081ba6ce75f639a1ff644d06bcf6a23e
Samandarjr10 <samandarjr123@gmail.com>
2021-11-04 11:34:25 +0500

    add js file

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// endi boshlandi
+var yourName = prompt("What is your name?");
+
+var yourSurname = prompt("What is your surname?");
+
+var yourCondition = prompt("How are you?");
+
+var yourAge = prompt("How old are you?");
+
+var yourCountry = prompt("Where are you from?");
+
+var yourCity = prompt("Where do you live?");
+
+var yourNationality = prompt("What is your nationality?");
+
+var yourJob = prompt("What is your job?");
+
+var yourLike = prompt("What do you like?");
+
+var yourHobby = prompt("What is your hobby?");
+
+var infofull =
+    "Name:" + " " + yourName + "\n" +
+    "Surname:" + " " + yourSurname + "\n" +
+    "Condition:" + " " + yourCondition + "\n" +
+    "Age:" + " " + yourAge + "\n" + 
+    "Country:" + " " + yourCountry + "\n" +
+    "City:" + " " + yourCity + "\n" +
+    "Nationality:" + " " + yourNationality + "\n" +
+    "Job:" + " " + yourJob + "\n" +
+    "Like:" + " " + yourLike + "\n" +
+    "Hobby:" + " " + yourHobby
+;
+
+console.log(infofull);
+
+alert(infofull);
+// tugadi
